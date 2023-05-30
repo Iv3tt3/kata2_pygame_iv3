7fb7775d73dcd0b5c802b657de4acb0b235301b2
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-05-30 19:08:56 +0100

    Minimo necesario con FPS

--- hello.py
+++ hello.py
@@ -1,20 +1,24 @@
+#Ver apuntes Notion
 import pygame
+pygame.init() 
 
-#Vamos a crear la pantalla, la ventana del juego
-pantalla = pygame.display.set_mode((800,600)) #Medida de la pantalla
-pygame.display.set_caption("Hola mundo") #Poner titulo a la pantalla
+pantalla = pygame.display.set_mode((800,600)) 
+pygame.display.set_caption("Hola mundo") 
 
-#Vamos a crear el bucle principal
+metronomo = pygame.time.Clock()
+
 game_over = False 
 azul = 0
 
-while game_over == False: #creamos una variable gameover en False y asi nos deja cerrar el bucle gracias al for siguiente
-    for evento in pygame.event.get(): #vaciar el bufer de eventos para que no te pete el pc
-        if evento.type == pygame.QUIT: #si el evento es cerrar, pones game over en True y asi sale del bucle
+while game_over == False: 
+    metronomo.tick(60) 
+    for evento in pygame.event.get(): 
+        if evento.type == pygame.QUIT: 
             game_over = True
     azul = azul + 1
     if azul > 255:
         azul = 0
+        
 
-    pantalla.fill((0,0,azul)) #pintamos los colores de azul con rgb
-    pygame.display.flip() #para que lo pase a la tarjeta grafica y de ahi al monitor
+    pantalla.fill((0,0,azul)) 
+    pygame.display.flip() 
